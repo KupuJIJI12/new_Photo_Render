8b605da95b622271aaef7034572684d999ad9ce5
alexzonic <smailik072001@mail.ru>
2021-02-02 16:30:28 +0400

    upd2

---
+++ photo-render.Api/Filters/ShadeFilter.cs
@@ -0,0 +1,23 @@
+﻿using ImageMagick;
+
+namespace photo_render.Api.Filters
+{
+    public class ShadeFilter : IFilter
+    {
+        private string _path;
+
+        public ShadeFilter(string path)
+        {
+            _path = path;
+        }
+        public string Filter()
+        {
+            using (var image = new MagickImage(_path))
+            {
+                image.Shade();
+                //image.Write(result);
+                return image.FileName;
+            }
+        }
+    }
+}
