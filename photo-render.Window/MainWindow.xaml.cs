cf80e01304103587bd69947ca36c05b33461ec5f
IPgyt01 <tyganovilya@gmail.com>
2021-02-06 00:49:17 +0400

    Исправление: очередное мелкое исправление. Присвоил _path имя импортированной картинки

--- photo-render.Window/MainWindow.xaml.cs
+++ photo-render.Window/MainWindow.xaml.cs
@@ -104,6 +104,7 @@ namespace photo_render.Window
             try
             {
                 image.Source = new BitmapImage(new Uri(inputImage[0]));
+                _path = inputImage[0];
                 listBox.IsEnabled = true;
             }
             catch
